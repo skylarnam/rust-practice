d36b4e175e2f6f1ff9f9420be1f0f20047c60fcd
Skylar Nam <rorelei@gmail.com>
2024-07-28 13:41:52 -0700

    Add more practice examples

--- methods/src/main.rs
+++ methods/src/main.rs
@@ -1,29 +1,4 @@
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
-
-    fn width(&self) -> bool {
-        self.width > 0
-    }
-
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width >= other.width && self.height >= other.height
-    }
-
-    fn square(size: u32) -> Self {
-        Self {
-            width: size,
-            height: size,
-        }
-    }
-}
+use rectangle::Rectangle;
 
 fn main() {
     let rect1 = Rectangle {
